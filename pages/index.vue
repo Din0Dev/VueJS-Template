f8ef99dee3fdeb62669cf056d43aed5da94d01c5
Din0Dev <honghai251.info@gmail.com>
2022-01-11 17:51:55 +0700

    Add-Template

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-layout-content class="container" :style="{ margin: '24px 16px 0', overflow: 'initial', minHeight: '100vh' }">
+    <div class="content">
+      Hello ! Im Dino Dev
+    </div>
+  </a-layout-content>
+</template>
+<script>
+export default {
+  methods: {
+    onCollapse(collapsed, type) {
+      console.log(collapsed, type);
+    },
+    onBreakpoint(broken) {
+      console.log(broken);
+    }
+  }
+};
+</script>
+
+<style>
+  .container{
+    min-height: 100vh;
+  }
+
+</style>
